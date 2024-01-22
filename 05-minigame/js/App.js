e72d7472dc5b673c1621452b8b35607572fa9bc1
injulme <injulme0309@naver.com>
2024-01-22 21:37:47 +0900

    feat: 05-minigame 완료

---
+++ 05-minigame/js/App.js
@@ -0,0 +1,132 @@
+import Background from './Background.js';
+import Coin from './Coin.js';
+import GameHandler from './GameHandler.js';
+import Player from './Player.js';
+import Score from './Score.js';
+import Wall from './Wall.js';
+
+export default class App {
+  static canvas = document.querySelector('canvas');
+  static ctx = App.canvas.getContext('2d');
+  static dpr = window.devicePixelRatio > 1 ? 2 : 1;
+  static interval = 1000 / 60;
+  static width = 1024;
+  static height = 768;
+
+  constructor() {
+    this.backgrounds = [
+      new Background({ img: document.querySelector('#bg3-img'), speed: -1 }),
+      new Background({ img: document.querySelector('#bg2-img'), speed: -2 }),
+      new Background({ img: document.querySelector('#bg1-img'), speed: -4 }),
+    ];
+    this.walls = [new Wall({ type: 'SMALL' })];
+    this.player = new Player();
+    this.coins = [];
+    this.score = new Score();
+    this.gameHandler = new GameHandler(this);
+    this.reset();
+  }
+
+  init() {
+    App.canvas.width = App.width * App.dpr;
+    App.canvas.height = App.height * App.dpr;
+    App.ctx.scale(App.dpr, App.dpr);
+    // 배경
+    this.backgrounds.forEach((background) => {
+      background.draw();
+    });
+  }
+
+  reset() {
+    this.walls = [new Wall({ type: 'SMALL' })];
+    this.player = new Player();
+    this.coins = [];
+    this.score = new Score();
+  }
+
+  render() {
+    let now, delta;
+    let then = Date.now();
+    const frame = () => {
+      requestAnimationFrame(frame);
+      now = Date.now();
+      delta = now - then;
+      if (delta < App.interval) return;
+
+      if (this.gameHandler.status !== 'PLAYING') return;
+
+      App.ctx.clearRect(0, 0, App.width, App.height);
+
+      // 배경
+      this.backgrounds.forEach((background) => {
+        background.update();
+        background.draw();
+      });
+
+      // 벽
+      for (let i = this.walls.length - 1; i >= 0; i--) {
+        this.walls[i].update();
+        this.walls[i].draw();
+
+        // 벽 제거
+        if (this.walls[i].isOutside) {
+          this.walls.splice(i, 1);
+          continue;
+        }
+
+        // 벽 생성
+        if (this.walls[i].canGenerateNext) {
+          this.walls[i].generatedNext = true;
+          const newWall = new Wall({ type: Math.random() > 0.3 ? 'SMALL' : 'BIG' });
+          this.walls.push(newWall);
+
+          // 코인 생성
+          if (Math.random() < 1) {
+            const x = newWall.x + newWall.width / 2;
+            const y = newWall.y2 - newWall.gapY / 2;
+            this.coins.push(new Coin(x, y, newWall.vx));
+          }
+        }
+
+        // 벽과 플레이어 충돌 감지
+        if (this.walls[i].isColliding(this.player.boundingBox)) {
+          this.gameHandler.status = 'FINISHED';
+          break;
+        }
+      }
+
+      // 플레이어
+      this.player.update();
+      this.player.draw();
+
+      if (this.player.y >= App.height || this.player.y + this.player.height <= 0) {
+        this.gameHandler.status = 'FINISHED';
+      }
+
+      // 코인
+      for (let i = this.coins.length - 1; i >= 0; i--) {
+        this.coins[i].update();
+        this.coins[i].draw();
+
+        if (this.coins[i].x + this.coins[i].width < 0) {
+          this.coins.splice(i, 1);
+          continue;
+        }
+
+        // 코인과 플레이어 충돌 감지
+        if (this.coins[i].boundingBox.isColliding(this.player.boundingBox)) {
+          this.coins.splice(i, 1);
+          this.score.coinCount += 1;
+        }
+      }
+
+      //점수
+      this.score.update();
+      this.score.draw();
+
+      then = now - (delta % App.interval);
+    };
+
+    requestAnimationFrame(frame);
+  }
+}
---
+++ 05-minigame/js/Background.js
@@ -0,0 +1,29 @@
+import App from './App.js';
+
+export default class Background {
+  constructor(config) {
+    this.img = config.img;
+    this.height = App.height;
+    this.width = App.height * (this.img.width / this.img.height);
+    this.leftPos = { x: 0, y: 0 };
+    this.rightPos = { x: this.width - 4, y: 0 }; // this.width 옆에 -4 한 이유: png 이미지가 조금 짧아서 잘리기 때문에 임의로 값을 설정함
+    this.speed = config.speed;
+  }
+
+  update() {
+    if (this.leftPos.x + this.width < 0) {
+      this.leftPos.x = this.rightPos.x + this.width - 4;
+    }
+    if (this.rightPos.x + this.width < 0) {
+      this.rightPos.x = this.leftPos.x + this.width - 4;
+    }
+
+    this.leftPos.x += this.speed;
+    this.rightPos.x += this.speed;
+  }
+
+  draw() {
+    App.ctx.drawImage(this.img, this.leftPos.x, this.leftPos.y, this.width, this.height);
+    App.ctx.drawImage(this.img, this.rightPos.x, this.rightPos.y, this.width, this.height);
+  }
+}
